f7a23113f5e43f168cf7371550825bf9283855fc
altafsyah <syahrastanialtaf@gmail.com>
2026-05-12 21:46:47 +0700

    feat: add preloader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,12 +7,14 @@ import { Faq } from "@/app/components/sections/faq";
 import { Reserve } from "@/app/components/sections/reserve";
 import { Footer } from "@/app/components/sections/footer";
 import { Toaster } from "@/app/components/ui/sonner";
+import { SplashScreen } from "@/app/components/ui/splash-screen";
 import { ReactLenis } from "lenis/react";
 import { BookingProvider } from "@/app/context/booking-context";
 
 export default function App() {
   return (
     <BookingProvider>
+      <SplashScreen />
       <ReactLenis root />
       <Hero />
       <About />
